aefe9623b09de5580dcc34e987e070e49dacbc71
manohar.sannamuri <sannamurimanohar1996@gmail.com>
2021-12-16 19:58:33 +0530

    else case added

--- demo.py
+++ demo.py
@@ -1,2 +1,12 @@
-print("Hello world")
-prinit("Save world")
+#print("Hello world")
+#prinit("Save world")
+
+number = int(input("Enter the number?"))
+if number==10:
+    print("number is equals to 10")
+elif number==50:
+    print("number is equal to 50");
+elif number==100:
+    print("number is equal to 100");
+else:
+    print("number is not equal to 10, 50 or 100");
