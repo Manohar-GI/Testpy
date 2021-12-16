e843e871d7d6eef45e54a4c360a4ceee3203487b
Manohar-GI <88126561+Manohar-GI@users.noreply.github.com>
2021-12-16 20:00:06 +0530

    ifcase stopped

--- demo.py
+++ demo.py
@@ -6,7 +6,7 @@ if number==10:
     print("number is equals to 10")
 elif number==50:
     print("number is equal to 50");
-elif number==100:
-    print("number is equal to 100");
+#elif number==100:
+   # print("number is equal to 100");
 else:
-    print("number is not equal to 10, 50 or 100");
+   print("number is not equal to 10, 50 or 100");
